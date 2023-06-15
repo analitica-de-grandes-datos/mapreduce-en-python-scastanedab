108f154fb8a890ad53c29c39607a4af3afb432d2
scastanedab <scastanedab@unal.edu.co>
2023-06-15 00:16:55 -0500

    9

--- pregunta_09/mapper.py
+++ pregunta_09/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columnas = linea.split("   ")
+        letra = columnas[0]
+        fecha = columnas[1]
+        valor = columnas[2]
+        sys.stdout.write(f"{letra},{fecha},{valor}\n")
--- pregunta_09/reducer.py
+++ pregunta_09/reducer.py
@@ -1,3 +1,29 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    dictionary = {}
+    count = 0
+    
+    for line in sys.stdin:
+        columna = line.split(",")
+        if len(columna) >= 3:
+            letra = columna[0]
+            date = columna[1]
+            num = int(columna[2])
+            
+            if num in dictionary:
+                if dictionary[num] > (letra, date):
+                    dictionary[num] = (letra, date)
+            else:
+                dictionary[num] = (letra, date)
+    
+    dictionary_sort = sorted(dictionary.items())
+    
+    for num, (letter, date) in dictionary_sort:
+        sys.stdout.write(f"{letter}   {date}   {num}\n")
+        count += 1
+        if count == 6:
+            break
