afd899d3dcb8838b4bc225aff342914287025da8
scastanedab <scastanedab@unal.edu.co>
2023-06-14 23:51:54 -0500

    3

--- pregunta_03/mapper.py
+++ pregunta_03/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    for row in sys.stdin:
+        clean = row.strip()
+        ltr, num = clean.split(',')
+
+        sys.stdout.write(f'{ltr}\t{num}\n')
--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import operator
+import sys
+
+if __name__ == '__main__':
+
+    dict = {}
+    
+
+    for line in sys.stdin:
+
+        key, val = line.split('\t')
+        val = int(val)
+        dict[key] = val
+
+    sortedDict = sorted(dict.items(), key = operator.itemgetter(1))
+    for line in sortedDict:
+              
+
+        sys.stdout.write('{},{}\n'.format(line[0], line[1]))
