2dc433a5fcf9915d20b0ffb510e2f10a93f39ee1
scastanedab <scastanedab@unal.edu.co>
2023-06-15 00:05:28 -0500

    6

--- pregunta_06/mapper.py
+++ pregunta_06/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    for linea in sys.stdin:
+        clean = linea.strip()
+        sys.stdout.write("{}\t{}\n".format(clean.split(" ")[0],clean.split("  ")[2]))
--- pregunta_06/reducer.py
+++ pregunta_06/reducer.py
@@ -1,3 +1,29 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    clave = None
+    maxx = None
+    minn = None
+    for linea in sys.stdin:
+
+        key, val = linea.split("\t")
+        val = float(val)
+
+        if key == clave:
+            if val > maxx:
+                maxx = val
+            if val < minn:
+                minn = val
+        else:
+            if clave is not None:
+                sys.stdout.write("{}\t{}\t{}\n".format(clave, maxx, minn))
+
+            clave = key
+            maxx = val
+            minn = val
+
+    sys.stdout.write("{}\t{}\t{}\n".format(clave, maxx, minn))
