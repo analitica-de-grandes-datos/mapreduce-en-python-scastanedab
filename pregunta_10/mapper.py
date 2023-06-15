b7b42ea721d0e30de4648e9011f869517be1bbf0
scastanedab <scastanedab@unal.edu.co>
2023-06-15 00:19:43 -0500

    10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+    for line in sys.stdin:
+        columns = line.strip().split("\t")
+        num = columns[0]
+        letters = columns[1].split(",")
+        for letter in letters:
+            sys.stdout.write(f"{num}\t{letter}\n")
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,26 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    dictionary = {}
+    
+    for line in sys.stdin:
+        columns = line.split("\t")
+        if len(columns) == 2:
+            num = columns[0]
+            letters = columns[1].strip()
+            
+            for letter in letters:
+                if letter in dictionary:
+                    dictionary[letter].append(int(num))
+                else:
+                    dictionary[letter] = [int(num)]
+
+    dictionary_sort = sorted(dictionary.items(), key=lambda x: x[0])
+    
+    for letter, values in dictionary_sort:
+        values_sorted = sorted(values)
+        values_str = ",".join(str(num) for num in values_sorted)
+        sys.stdout.write(f"{letter}\t{values_str}\n")
