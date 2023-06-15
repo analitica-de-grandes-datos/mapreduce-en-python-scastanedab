24608672bb410c6e66b44a5411de28c7f8960d75
scastanedab <scastanedab@unal.edu.co>
2023-06-14 23:36:29 -0500

    Update mapper.py
    Pregunta 01

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,9 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "main":
+    for line in sys.stdin:
+    line = line.strip()
+    splits = line.split(',')
+    print(splits[2] + '\t' + '1')
