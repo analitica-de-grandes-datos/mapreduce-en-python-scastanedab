0d7d980df93f9a3bffaaf98b915453d6d73d21ac
scastanedab <scastanedab@unal.edu.co>
2023-06-15 00:12:11 -0500

    8

--- pregunta_08/mapper.py
+++ pregunta_08/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    for linea in sys.stdin:
+        columnas = linea.split("   ")
+        letra = columnas[0]
+        valor = columnas[2]
+        sys.stdout.write(f"{letra},{valor}\n")
--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+    dictionary = {}
+    for line in sys.stdin:
+        columns = line.split(",")
+        if len(columns) == 2:
+            letter = columns[0]
+            amount = float(columns[1])
+            if letter in dictionary:
+                dictionary[letter][0] += amount
+                dictionary[letter][1] += 1
+            else:
+                dictionary[letter] = [amount, 1]
+    
+    dictionary_sort = sorted(dictionary.items(), key=lambda x: x[0])
+    for atributo, valores in dictionary_sort:
+        suma = valores[0]
+        contador = valores[1]
+        promedio = suma / contador
+        sys.stdout.write(f"{atributo}\t{suma}\t{promedio}\n")
