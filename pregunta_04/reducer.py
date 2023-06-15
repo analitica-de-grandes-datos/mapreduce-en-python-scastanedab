99759dd85ae42d3a2aca86aca20c91330dcdd96b
scastanedab <scastanedab@unal.edu.co>
2023-06-14 23:57:59 -0500

    4

--- pregunta_04/reducer.py
+++ pregunta_04/reducer.py
@@ -1,3 +1,24 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    clave = None
+    tot = 0
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = int(val)
+
+        if key == clave:
+            tot += val
+        else:
+            if clave is not None:
+                sys.stdout.write("{},{}\n".format(clave, tot))
+
+            clave = key
+            tot = val
+
+    sys.stdout.write("{},{}\n".format(clave, tot))
