20b285f7374f4a0fee9a6e49e315e1e11722dcff
scastanedab <scastanedab@unal.edu.co>
2023-06-15 00:08:52 -0500

    7

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,10 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        clean =   line.strip()
+        key, val1, val2 = clean.split("   ")
+        sys.stdout.write("{},{},{}\n".format(key,val1,val2))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,20 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+
+if __name__ == '__main__':
+
+    orden= [] 
+    for line in sys.stdin:
+
+        key, val1, val2 = line.strip().split(",")
+        val2 = int(val2)
+        orden.append([key,val1,val2])
+    
+
+    elements = sorted(orden, key =lambda x:(x[0],x[2]))
+    
+    for element in elements:
+        sys.stdout.write("{}   {}   {}\n".format(element[0],element[1],element[2]))
